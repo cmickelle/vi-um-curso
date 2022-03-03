340223f71d9ed2d4faa7470c18956dfe6bca4df8
Christopher Mickelle <94025010+cmickelle@users.noreply.github.com>
2022-03-03 16:16:04 -0300

    Codbase (#13)
    * como não refazer tweet do próprio tweet, duplicidade do tweet e lista de exclusão. resolve #5 e #7
    
    * corrigindo commit feito errado

--- main.py
+++ main.py
@@ -11,20 +11,16 @@ Token = getenv('access_token')
 TokenSecret = getenv('access_token_secret')
 Query = getenv('QUERY')
 Like = getenv('LIKE')
-Follow = getenv('FOLLOW')
 SleepTime = int(getenv('SLEEP_TIME'))
-UserIdViUmCurso = 1646786510  # id de teste
-
+UserIdViUmCurso = getenv('UserIdViUmCurso')
+ListaDeExclusao = getenv('ListaDeExclusao')
 
 if Like in ["True", 'TRUE', 'TrUe', 'Verdadeiro']:  # statemant
     NewLike = True
 else:
     NewLike = False
 
-if Follow in ["True", 'TRUE', 'TrUe', 'Verdadeiro']:
-    NewFollow = True
-else:
-    NewFollow = False
+
 
 
 auth = tweepy.OAuthHandler(Key, Secret)
@@ -35,9 +31,7 @@ api = tweepy.API(auth)
 print("Twitter bot which retweets, like tweets and follow users")
 print("Bot Settings")
 print('Like Tweets: {}'.format(NewLike))
-print('Follow users: {}'.format(NewFollow))
-# print("Like Tweets :", like)
-# print("Follow users :", Follow)
+
 
 
 for tweet in tweepy.Cursor(api.search_tweets, q=Query).items():  # statemant
@@ -49,33 +43,41 @@ for tweet in tweepy.Cursor(api.search_tweets, q=Query).items():  # statemant
     print(UserId['id'])
 
     if RetweetStatus != "null":
-        tweet = api.get_status(RetweetStatus)
-        RetweetStatus = tweet._json['in_reply_to_status_id']
-        Id = tweet._json['id']
-        CreationTime = tweet._json['created_at']
-        Text = tweet._json['text']
-        UserId = tweet._json['user']
-        print(UserId['id'])
+        try:
+            tweet = api.get_status(RetweetStatus)
+            RetweetStatus = tweet._json['in_reply_to_status_id']
+            Id = tweet._json['id']
+            CreationTime = tweet._json['created_at']
+            Text = tweet._json['text']
+            UserId = tweet._json['user']
+            print(UserId['id'])
+        except tweepy.errors.NotFound:
+            pass
 
-    if UserId['id'] == UserIdViUmCurso:
-        break
+        if UserId['id_str'] == UserIdViUmCurso:
+            continue
 
-    try:  # tratamento de exceção
-        print('\nTweet by: @' + tweet.user.screen_name)
-        # tweet.retweet()
-        print(tweet)
-        print('Retweeted the tweet')
-        if NewLike:
-            # tweet.favorite()
-            print('Favorited the tweet')
-        if NewFollow:
-            if not tweet.user.following:
-                # tweet.user.follow()
-                print('Followed the user')
-        sleep(SleepTime)
+        for exclusao in ListaDeExclusao:
+            if exclusao not in Text:
+                run = True
+                continue
+            else:
+                run = False
+                break
 
-    except tweepy.TweepyException as e:
-        print(e)
+        if run:
+            try:  # tratamento de exceção
+                print('\nTweet by: @' + tweet.user.screen_name)
+                tweet.retweet()
+                print('Retweeted the tweet')
+                if NewLike:
+                    tweet.favorite()
+                    print('Favorited the tweet')
+                sleep(SleepTime)
 
-    except StopIteration:
-        break
+            except tweepy.TweepyException as e:
+                print(e)
+
+            except StopIteration:
+                break
+
